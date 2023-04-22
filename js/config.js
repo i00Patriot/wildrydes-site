6acfae9513cfae661955cdb4713a922ed1b87ac9
i00Patriot <94723245+i00Patriot@users.noreply.github.com>
2023-04-22 16:00:54 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_U9DHl8cbe',         // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1p6r40ufa3tped5vgffpmshkp0',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        region: 'us-east-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
